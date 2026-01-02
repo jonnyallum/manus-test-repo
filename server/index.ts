1e5b944d6e59a6a1f448469b4b33ca0135ead35e
Jonny Allum <jonnyallum@gmail.com>
2026-01-02 14:22:43 +0000

    Refactor server index to simplify port handling

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,1 @@
-import { findAvailablePort } from "./_core/index.js";
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-
-findAvailablePort(PORT)
-  .then((port) => {
-    console.log(`Server started on port ${port}`);
-  })
-  .catch((error) => {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  });
+import "./_core/index.js";
